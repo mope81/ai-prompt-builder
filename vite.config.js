f1821cbbff685f82968da0bd816a2e8383123fa6
mope81 <mope81@github.com>
2024-12-01 17:24:16 +0800

    Update vite.config.js
    Fix: Add base config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',  // 允许外部访问
     port: 5173,       // 指定端口
